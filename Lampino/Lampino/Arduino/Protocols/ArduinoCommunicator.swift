bce096140efd50ec5eabea4047fbc98744980eef
Victor Melo <victor.contas@outlook.com>
2018-02-20 14:37:41 -0300

    Created the project structure

---
+++ Lampino/Lampino/Arduino/Protocols/ArduinoCommunicator.swift
@@ -0,0 +1,13 @@
+//
+//  ArduinoCommunicator.swift
+//  Lampino
+//
+//  Created by Victor S Melo on 20/02/18.
+//  Copyright © 2018 Victor S Melo. All rights reserved.
+//
+
+import Foundation
+
+protocol ArduinoCommunicator {
+    
+}
