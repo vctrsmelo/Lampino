8799d7e9c76468ec8602cd26d327fd05fed6ffd6
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2018-02-27 17:12:00 -0300

    Removed commented code

--- Lampino/Arduino/Lampino/Lampino.ino
+++ Lampino/Arduino/Lampino/Lampino.ino
@@ -1,27 +1,17 @@
 #include "LampController.hpp"
-//
-const int arraySize = 3;
-// 
+
 byte redLed = 11;
 byte yellowLed = 10;
 byte greenLed = 9;
-// 
-//int brightness = 0;    // how bright the LED is
-byte byteRead = 0;
-//int selectedLed = -1;
-// 
+
+const int arraySize = 3;
 byte leds[arraySize] = {redLed, yellowLed, greenLed};  
-// 
-//const char communicationProtocol = 'X';
- 
-// the setup routine runs once when you press reset
+
 LampController::Controller controller(leds, arraySize);
 
+byte byteRead = 0;
+
 void setup() {
-  // declare pin 9 to be an output:
-//  pinMode(redLed, OUTPUT);
-//  pinMode(yellowLed, OUTPUT);
-//  pinMode(greenLed, OUTPUT);
   Serial.begin(115200);
   Serial.flush();
 }
@@ -29,23 +19,6 @@ void setup() {
 // the loop routine runs over and over again forever
 void loop() {
     while(Serial.available() > 0 ) {
-      
-        byteRead = Serial.read();
-
-        controller.receivedByte(byteRead);
- 
-//        Serial.println(byteRead);
- 
-//        if ( byteRead == communicationProtocol ) {
-//           Serial.println(sizeOfArray(leds));
-//        }
- 
-//        if (selectedLed != -1) {
-//          analogWrite(selectedLed, byteRead);
-//          selectedLed = -1;
-//        } else if ( byteRead < arraySize ) {
-//          selectedLed = leds[byteRead];
-//        }
+        controller.receivedByte(Serial.read());
     }
-   //delay(50);
 }
