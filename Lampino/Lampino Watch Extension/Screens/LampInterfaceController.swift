fc3ee52aa6774250c928afe1d4036cc4c98088a8
Rodrigo Cardoso Buske <rodbuske@gmail.com>
2018-03-08 19:17:04 -0300

    Added support for crown and fixes some bugs

--- Lampino/Lampino Watch Extension/Screens/LampInterfaceController.swift
+++ Lampino/Lampino Watch Extension/Screens/LampInterfaceController.swift
@@ -8,52 +8,39 @@
 
 import WatchKit
 
-class WKLampsManager {
-    
-    /// is true when the watch extension has already loaded the lamps into UI.
-    static var alreadyLoaded = false
-}
-
 class LampInterfaceController: WKInterfaceController {
     
-    @IBOutlet var nameLabel: WKInterfaceLabel!
-    @IBOutlet var onOffButton: WKInterfaceButton!
-    @IBOutlet var brightnessSlider: WKInterfaceSlider!
+    @IBOutlet private weak var noLampsLabel: WKInterfaceLabel!
+    @IBOutlet private weak var lampGroup: WKInterfaceGroup!
     
-    private var _currentBrightnessValue: Float!
+    @IBOutlet private weak var nameLabel: WKInterfaceLabel!
+    @IBOutlet private weak var onOffButton: WKInterfaceButton!
+    @IBOutlet private weak var brightnessSlider: WKInterfaceSlider!
+    
+    private var _currentBrightnessValue: Float = 0
+    private var lampId: UInt8 = 0
     
     private var isOn: Bool! {
         didSet {
-            if oldValue == isOn {
-                return
-            }
             
             if isOn {
                 onOffButton.setBackgroundImageNamed("ON")
                 brightnessSlider.setColor(UIColor.onColor)
                 
-
-                if var _currentBrightnessValue = _currentBrightnessValue {
-                    if _currentBrightnessValue == 0 {
-                        _currentBrightnessValue = 10
-                        brightnessSlider.setValue(_currentBrightnessValue)
-                    }
-                    lamp.brightness = UInt8(_currentBrightnessValue*Float(Lamp.maxBrightness)/10.0)
-                    LampsManager.sharedInstance.setBrightness(lamp.brightness, to: lamp.id)
+                if _currentBrightnessValue == 0 {
+                    _currentBrightnessValue = 10
+                    brightnessSlider.setValue(_currentBrightnessValue)
                 }
+                
+                let brightness = UInt8(_currentBrightnessValue * Float(Lamp.maxBrightness) / 10.0)
+                LampsManager.sharedInstance.setBrightness(brightness, to: self.lampId)
                 
             } else {
                 onOffButton.setBackgroundImageNamed("OFF")
                 brightnessSlider.setColor(UIColor.offColor)
-                lamp.brightness = 0
-                LampsManager.sharedInstance.setBrightness(lamp.brightness, to: lamp.id)
+                let brightness = UInt8(0)
+                LampsManager.sharedInstance.setBrightness(brightness, to: self.lampId)
             }
-        }
-    }
-    
-    var lamp: Lamp! {
-        didSet {
-            nameLabel.setText(lamp.name)
         }
     }
 
@@ -61,40 +48,55 @@ class LampInterfaceController: WKInterfaceController {
         super.awake(withContext: context)
         
         self.setTitle("Lamps")
-        
+        crownSequencer.delegate = self
+        LampsManager.sharedInstance.delegate = self
+                
         if let lamp = context as? Lamp {
-            self.lamp = lamp
-            _currentBrightnessValue = Float(lamp.brightnessPercentage)/10.0
+            self.noLampsLabel.setHidden(true)
+            self.lampGroup.setHidden(false)
+            
+            self.lampId = lamp.id
+            nameLabel.setText(lamp.name)
+            
+            _currentBrightnessValue = Float(lamp.brightnessPercentage) / 10.0
             brightnessSlider.setValue(_currentBrightnessValue)
             isOn = (lamp.brightness > 0)
+            
+        } else {
+            self.noLampsLabel.setHidden(false)
+            self.lampGroup.setHidden(true)
         }
-        
-        if WKLampsManager.alreadyLoaded {
-            return
-        }
-        
-        WKLampsManager.alreadyLoaded = true
-        
-        let namesAndContexts = LampsManager.sharedInstance.lamps.map { return (name: "LampPage", context: $0 as AnyObject)}
-        WKInterfaceController.reloadRootControllers(withNamesAndContexts: namesAndContexts)
-
     }
     
     override func willActivate() {
         super.willActivate()
+        
+        crownSequencer.focus()
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+    private func reloadData() {
+        print(#function)
+        let namesAndContexts = LampsManager.sharedInstance.lamps.map { return (name: "LampPage", context: $0 as AnyObject)}
+        
+        if namesAndContexts.isEmpty {
+            WKInterfaceController.reloadRootControllers(withNamesAndContexts: [(name: "LampPage", context: "nothing" as AnyObject)])
+            
+        } else {
+            WKInterfaceController.reloadRootControllers(withNamesAndContexts: namesAndContexts)
+        }
     }
     
     @IBAction func didChangeSlider(_ value: Float) {
+        print(value)
         _currentBrightnessValue = value
         isOn = (value != 0.0)
     }
     
     @IBAction func onOffButtonTouched() {
-        
         isOn = !isOn
     }
 }
@@ -106,5 +108,37 @@ extension UIColor {
     
     static var offColor: UIColor {
         return UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1)
+    }
+}
+
+extension LampInterfaceController: WKCrownDelegate {
+    func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
+        let currentValue = self._currentBrightnessValue
+        
+        if rotationalDelta > 0.1 && currentValue < 10 {
+            self.brightnessSlider.setValue(currentValue + 1)
+            self.didChangeSlider(currentValue + 1)
+            
+        } else if rotationalDelta < -0.1 && currentValue > 0 {
+            self.brightnessSlider.setValue(currentValue - 1)
+            self.didChangeSlider(currentValue - 1)
+        }
+    }
+}
+
+extension LampInterfaceController: LampsManagerDelegate {
+    func didConnect() {
+        print(#function)
+        self.reloadData()
+    }
+    
+    func didDisconnect() {
+        print(#function)
+        self.reloadData()
+    }
+    
+    func didReceiveNewBrightness() {
+        print(#function)
+        self.reloadData()
     }
 }
